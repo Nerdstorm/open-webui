dbf510efcde1291f0477b44be8c728d2035b41eb
Purinda Gunasekara <purinda@gmail.com>
2024-11-26 09:12:38 +1100

    New Chat and Workspace links working

--- src/lib/components/layout/Sidebar.svelte
+++ src/lib/components/layout/Sidebar.svelte
@@ -418,11 +418,11 @@
 			<a
 				id="sidebar-new-chat-button"
 				class="flex flex-1 rounded-lg px-2 py-1 h-full hover:bg-gray-100 dark:hover:bg-gray-900 transition"
-				href="/"
+				href="{WEBUI_BASE_URL}/"
 				draggable="false"
 				on:click={async () => {
 					selectedChatId = null;
-					await goto('/assistant/');
+					await goto(WEBUI_BASE_URL + '/');
 					const newChatButton = document.getElementById('new-chat-button');
 					setTimeout(() => {
 						newChatButton?.click();
@@ -474,7 +474,7 @@
 			<div class="px-1.5 flex justify-center text-gray-800 dark:text-gray-200">
 				<a
 					class="flex-grow flex space-x-3 rounded-lg px-2 py-[7px] hover:bg-gray-100 dark:hover:bg-gray-900 transition"
-					href="/workspace"
+					href="{WEBUI_BASE_URL}/workspace"
 					on:click={() => {
 						selectedChatId = null;
 						chatId.set('');
